29e2931440dd9dd5d8c4da694fc5678f7f819e60
hypha <ramble.dalloway@gmail.com>
2017-03-13 23:24:21 +0000

    added pop-up infowondow

--- zoopla/map.py
+++ zoopla/map.py
@@ -1,3 +1,4 @@
+import random
 class Map(object):
     def __init__(self):
         self._points = []
@@ -5,17 +6,21 @@ class Map(object):
     def add_point(self, coordinates):
         self._points.append(coordinates)
 
-    def marker_js(self, latitude=0,longtitude=0, target_url="http://www.planewalk.net", description="unknown property"):
-        return """var marker = new google.maps.Marker({{
+    def marker_js(self, latitude=0,longtitude=0, target_url="http://www.planewalk.net", description="unknown property", id=500):
+        return """var marker{id} = new google.maps.Marker({{
                 position: new google.maps.LatLng({lat}, {lon}),
                 map: map,
                 optimized:false,
                 title: {desc},
                 url: "{url}"
                 }});
-                marker.addListener('click', function() {{
-                    window.open(this.url);
-                }});""".format(lat=latitude, lon=longtitude, url=target_url, desc=description)
+                var infowindow{id} = new google.maps.InfoWindow();
+
+                 google.maps.event.addListener(marker{id}, 'click', function(evt) {{
+                    infowindow{id}.setContent(marker{id}.title)
+                    infowindow{id}.open(map,marker{id})
+                 }});
+                """.format(lat=latitude, lon=longtitude, url=target_url, desc=description, id=id)
 
     def __str__(self):
         centerLat = sum((x[0] for x in self._points)) / len(self._points)
@@ -26,7 +31,10 @@ class Map(object):
             #     map: map,
             #     title: <a href="{url}">Hello World!</a>
             #     }});"""
-            [self.marker_js(latitude=x[0], longtitude=x[1], target_url=x[2], description=x[3]) for x in self._points])
+
+            [self.marker_js(latitude=x[0], longtitude=x[1], target_url=x[2], description=x[3], id=self._points.index(x))for x in self._points])
+        print type(self._points)
+        print
         return """
             <script src="https://maps.googleapis.com/maps/api/js?key={key}&v=3.exp"></script>
             <div id="map-canvas" style="height: 100%; width: 100%"></div>
